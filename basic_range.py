d71e4473280d6c8b9d465db756df71b5ffa65478
Alfred Wong <wyfalfred@gmail.com>
2022-06-27 00:12:21 -0500

    first commit

---
+++ basic_range.py
@@ -0,0 +1,6 @@
+import random
+# random is default function in python, generate random number
+
+for i in range(100):
+	r = random.randint(1, 1000)
+	print("it is the", i + 1, "times random number generated")
